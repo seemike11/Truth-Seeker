206531e309eafcdaf2d4001b5ee1f30ed596f8d4
seemike11 <seanmichaels1189@gmail.com>
2026-01-23 01:29:26 -0500

    Add fact check endpoint and UI

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { UserController } from '../controllers';
+import { FactCheckController, UserController } from '../controllers';
 
 const router = Router();
 const userController = new UserController();
+const factCheckController = new FactCheckController();
 
 router.post('/users', userController.createUser.bind(userController));
 router.get('/users/:id', userController.getUser.bind(userController));
+router.post('/fact-check', factCheckController.checkStatement.bind(factCheckController));
 
 export default router;
